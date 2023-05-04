741d719bdc4cb23862725e7fe0d0156ca4da2596
vutiendinh <vutiendinh1998@gmail.com>
2023-05-05 00:05:53 +0700

    5/5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import TodoFeature from "./features/Todo";
+import { NavLink, Redirect, Route, Switch } from 'react-router-dom';
+import TodoFeature from './features/Todo';
+import AlbumFeature from './features/Album';
+import NotFound from './components/NotFound';
 
 function App() {
 
   return (
     <div className="App">
-      <TodoFeature />
+      Header
+      <p>
+        <NavLink to= '/todos' activeClassName='active-menu'>Todos</NavLink>
+      </p>    
+      <p>
+        <NavLink to= '/albums'>Albums</NavLink>
+      </p>
+    <Switch>
+      <Redirect from fro='/home' tot='/' exact/>
+      <Redirect from='/post-list/:postId' to='posts/:postId' exact/>
+
+      <Route path='/' component={TodoFeature} exact/>
+      <Route path='/todos' component={TodoFeature}/>
+      <Route path='/albums' component={AlbumFeature}/>
+      <Route component={NotFound} />
+    </Switch>
+      Footer
     </div>
   );
 }
--- src/features/Todo/index.jsx
+++ src/features/Todo/index.jsx
@@ -1,66 +1,22 @@
 import React, { useState } from 'react';
-import TodoList from './components/TodoList';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import ListPage from './pages/ListPage';
+import DetailPage from './pages/DetailPage';
 
 TodoFeature.propTypes = {
     
 };
 
 function TodoFeature(props) {
-    const initTodoList = [
-        {
-            id: 1,
-            title: 'Eat',
-            status: 'new'
-        },
-        {
-            id: 2,
-            title: 'Code',
-            status: 'completed'
-        },
-        {
-            id: 3,
-            title: 'Sleep',
-            status: 'new'
-        }
-    ]
-    const [todoList, setTodoList] = useState(initTodoList)
-    const [filterStatus, setFilterStatus] = useState('all')
+    const match = useRouteMatch();
 
-    const handleTodoClick = (todo, index) => {
-        //clone current array to the new one
-        const newTotoList = [...todoList]
-        
-        //toggle state
-        const newTodo = {
-            ...newTotoList[index],
-            status: newTotoList[index].status === 'new' ? 'completed' : 'new',
-        }
-        //update todoList
-        newTotoList[index] = newTodo
 
-        setTodoList(newTotoList)
-    }
-
-    const handleShowAllClick = () => {
-        setFilterStatus('all')
-    }
-    const handleShowCompletedClick = () => {
-        setFilterStatus('completed')
-    }
-    const handleShowNewClick = () => {
-        setFilterStatus('new')
-    }
-
-    const renderTodoList = todoList.filter(todo => filterStatus === 'all' || filterStatus === todo.status)
     return (
         <div>
-            <h3>Todo List</h3>
-            <TodoList onTodoClick={handleTodoClick} todoList={renderTodoList}/>
-            <div>
-                <button onClick={handleShowAllClick}>Show All</button>
-                <button onClick={handleShowCompletedClick}>Show Completed</button>
-                <button onClick={handleShowNewClick}>Show New</button>
-            </div>
+           <Switch>
+                <Route path={match.path} component={ListPage} exact />
+                <Route path={`${match.path}/:todoId`} component={DetailPage} />
+           </Switch>
         </div>
     );
 }
